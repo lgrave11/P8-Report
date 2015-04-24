0d7db946e2accf028327e3d4bd13b8dd3d6c4ee8
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2015-04-24 10:50:48 +0200

    Ret plots til søvn estimering.

---
+++ Report/grafik/amplitude-plot.py
@@ -0,0 +1,26 @@
+import datetime
+import matplotlib
+import matplotlib.dates
+from matplotlib.dates import YearLocator, MonthLocator, DateFormatter
+
+def main():
+    f = open(r"C:\Users\Mink\Desktop\sdf(1).txt", "r").readlines()
+    lst = [(datetime.datetime.strptime(x.split("\t")[0].strip(), "%Y-%m-%d %H:%M:%S.%f"), x.split("\t")[1]) for x in f]
+    lst2 = zip(*lst)
+    format = matplotlib.dates.DateFormatter('%H:%M')
+
+    dates = matplotlib.dates.date2num(lst2[0])
+    matplotlib.use('PDF')
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots()
+    ax.plot_date(dates, lst2[1], 'o-', color="red", markerfacecolor='red', markeredgecolor='red', markersize=2, label="Amplitude", rasterized=True)
+    plt.legend(["Amplitude"])
+    ax.xaxis.set_major_formatter(format)
+    ax.autoscale_view()
+    ax.grid(True)
+    fig.autofmt_xdate()
+    plt.savefig(r"C:\Users\Mink\Desktop\sdf.pdf", dpi=400)
+
+if __name__ == '__main__':
+    main()
